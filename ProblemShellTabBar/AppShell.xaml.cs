f7d92865a3e4062c3a305feb28e0ebad7160080d
MaksimAdamenko <63460828+MaksimAdamenko@users.noreply.github.com>
2022-10-29 19:51:05 +0300

    Add project files.

---
+++ ProblemShellTabBar/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ProblemShellTabBar;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ProblemShellTabBar/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ProblemShellTabBar;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
